e48c4083175170c1bc3a998d7c607681650d9e0e
zzzhp <www.957660233@qq.com>
2018-08-14 11:16:03 +0800

    springboot——demo

---
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping("/Hello")
+	public String Hello() {
+		return "Hello World";
+	}
+}
---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.JSONResult;
+import com.example.demo.domain.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@RequestMapping("/getuser")
+	public JSONResult getUser() {
+		User u = new User();
+		u.setName("张三");
+		u.setPassword("123");
+		u.setAge(18);
+		u.setBirthday(new Date());
+		u.setDesc("大帅比");
+		return JSONResult.ok(u);
+	}
+}
