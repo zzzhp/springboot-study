0d64b93b0dc9630f5c70c7a60cc177c6fe38c27f
zzzhp <www.957660233@qq.com>
2018-08-15 16:26:31 +0800

    修改服务器以及Tomcat默认配置

--- demo/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,12 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.JSONResult;
+import com.example.demo.domain.Resource;
+
 @RestController
 public class HelloWorldController {
+	@Autowired
+	private Resource resource;
 	@RequestMapping("/Hello")
 	public String Hello() {
 		return "Hello World";
+	}
+	@RequestMapping("/getresource")
+	public JSONResult resource() {
+		Resource bean = new Resource();
+		BeanUtils.copyProperties(resource, bean);
+		return JSONResult.ok(bean);
 	}
 }
